1e5ba18265303d37ce6fe5e292ea39ea1fe64951
Joshua Veit <javeit@ostatemail.okstate.edu>
2018-04-08 21:56:21 -0500

    First Iteration of CarController
    Added the AICarGuide and AICarController scripts
    Added the CarControllerTestScene

---
+++ Assets/_Red Team/Scripts/AI Cars/AICarController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RedTeam {
+
+	[RequireComponent(typeof(NavMeshAgent))]
+	public class AICarController : MonoBehaviour {
+
+		public GameObject guide;
+
+		NavMeshAgent agent;
+
+		void Update() {
+			agent.SetDestination(guide.transform.position);
+		}
+
+		void Awake() {
+			agent = GetComponent<NavMeshAgent>();
+		}
+	}
+}
---
+++ Assets/_Red Team/Scripts/AI Cars/AICarGuide.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RedTeam {
+
+	public class AICarGuide : MonoBehaviour {
+
+		public Waypoint startWaypoint;
+		public float speed;
+		public GameObject car;
+		public float followDistance;
+
+		IWaypoint waypoint;
+
+		void Update() {
+
+			// make sure the guide keeps within the given following distance of the car
+			if((car.transform.position - transform.position).magnitude <= followDistance) {
+
+				Vector3 remainingDistance = waypoint.Position - transform.position;
+
+				if(remainingDistance.magnitude <= waypoint.ArrivedDistance) {
+				
+					if(waypoint.GetNext() != null)
+						waypoint = waypoint.GetNext();
+				
+				} else if(remainingDistance.magnitude <= speed) {
+				
+					transform.Translate(remainingDistance);
+
+				} else {
+				
+					transform.Translate(remainingDistance.normalized * speed);
+
+				}
+			}
+		}
+
+		void Awake() {
+			waypoint = startWaypoint;
+		}
+	}
+}
