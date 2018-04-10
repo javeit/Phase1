6de9fc6f9aaea8523662f09d0d75caf6deed75d0
javeit <joshuaveit1995@gmail.com>
2018-04-10 10:03:24 -0500

    Updating the CarController Movement and Added WaypointManager
    Updated the AICarGuide script to move more smoothly
    Added a WaypointManager script to hold references to all the waypoint
    objects so that the AICarGuides can easily get a reference to the
    nearest waypoint to their starting position

---
+++ Assets/_Red Team/Scripts/Waypoints/WaypointManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RedTeam {
+
+	public class WaypointManager : MonoBehaviour {
+
+		Waypoint[] waypoints;
+		Waypoint[] Waypoints {
+			get {
+				if (waypoints == null) {
+					waypoints = GetComponentsInChildren<Waypoint> ();
+				}
+				return waypoints;
+			}
+		}
+
+		public IWaypoint GetWaypointNear(Vector3 position) {
+			if (Waypoints == null || Waypoints.Length == 0)
+				return null;
+
+			Waypoint nearestWaypoint = Waypoints [0];
+
+			foreach (Waypoint waypoint in Waypoints) {
+				if ((waypoint.Position - position).magnitude < (nearestWaypoint.Position - position).magnitude)
+					nearestWaypoint = waypoint;
+			}
+
+			return nearestWaypoint;
+		}
+	}
+}
